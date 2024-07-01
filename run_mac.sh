1ff16d71c6a6dd5495a1a8a15d80797186f564d8
Kerem Okyay <kerem13okyay@gmail.com>
2024-07-01 09:05:37 +0200

    remove some files

--- run_mac.sh
+++
@@ -1,8 +0,0 @@
-#gradle run --args="-parser refactor.lang" &&
-#java refactor &&
-#
-#gradle run --args="-parser file.lang" &&
-#java file
-
-gradle run --args="-parser refactor.lang" &&
-java refactor
